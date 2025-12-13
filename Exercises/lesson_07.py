eab8605f7426f0770e8430623a5657c085d70844
Cosmin <floreacosmin06@gmail.com>
2025-12-13 19:42:36 +0000

    Lesson_07_done

--- Exercises/lesson_07.py
+++ Exercises/lesson_07.py
@@ -2,15 +2,12 @@
 # restituisca una NUOVA lista ordinata per lunghezza crescente
 # e, a parità di lunghezza, per ordine alfabetico case-insensitive.
 # Usa una funzione anonima (lambda) come parametro key di sorted.
-
 def esercizio1(lista_stringhe):
-    pass
-
+    return sorted(lista_stringhe, key = lambda l: (len(l), l.lower()))
 
 print(esercizio1(["cane", "Gatto", "a", "Casa", "AA"]))
 print(esercizio1([]))
 print(esercizio1(["x", "X", "xx", "XX", "xxx", "XXX"]))
-
 
 
 # esercizio2: scrivi una funzione che, data una lista di interi,
@@ -18,15 +15,12 @@ print(esercizio1(["x", "X", "xx", "XX", "xxx", "XXX"]))
 # - le chiavi sono SOLO i numeri pari della lista
 # - i valori sono il quadrato del numero
 # Usa una dict comprehension con condizione if.
-
 def esercizio2(lista_interi):
-    pass
-
+    return {n: n**2 for n in lista_interi if n % 2 == 0}
 
 print(esercizio2([1, 2, 3, 4, 5, 6]))
 print(esercizio2([]))
 print(esercizio2([0, -2, -3, 10, 11]))
-
 
 
 # esercizio3: scrivi una funzione che, data una lista L di numeri
@@ -35,10 +29,15 @@ print(esercizio2([0, -2, -3, 10, 11]))
 # - Implementa una funzione interna estrai_massimo(lista) che rimuove
 #   e restituisce il massimo da quella lista.
 # - Usa una list comprehension per chiamare estrai_massimo k volte.
-
 def esercizio3(L, k):
-    pass
-
+    assert L, f"Lista {L} vuota"
+    assert 0 < k <= len(L), f"Valore {k} non valido"
+    copia = L.copy()
+    def estrai_massimo(lista):
+        m = max(lista)
+        lista.remove(m)
+        return m
+    return [estrai_massimo(copia) for _ in range(k)]
 
 print(esercizio3([1, 5, 2, 89, 2, 23], 2))
 print(esercizio3([10, 9, 8, 7], 4))
